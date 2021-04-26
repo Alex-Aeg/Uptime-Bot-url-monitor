705a82609ec1494a1b1ab7006ce71376d151b8b4
Alex-Aeg <alx@aegialis.com>
2021-04-26 13:44:32 +0000

    fix

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 const urlmon = require('url-monitor');
+const nodemailer = require('nodemailer');
+
+
+
+var transporter = nodemailer.createTransport({
+ host: "smtp.example.com",
+  port: 587,
+  secure: true, // upgrade later with STARTTLS
+  auth: {
+    user: "username",
+    pass: "password"
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 
 const website = new urlmon({
 	url:'https://amorgos-aegialis.com/9847298579482759043705702934', 
@@ -8,7 +36,14 @@ const website = new urlmon({
 
 website.on('error', (data) => {
     //website.stop();
-	console.log(data);
+    console.log(data);
+    transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 })
 
 website.on('available', (data) => {
