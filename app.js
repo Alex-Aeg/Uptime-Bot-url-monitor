40c752527798c654df29dc8259038d2aa80a6f61
Alex-Aeg <alx@aegialis.com>
2021-04-26 14:11:10 +0000

    hetz

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const urlmon = require('url-monitor');
 var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'youremail@gmail.com',
-    pass: 'yourpassword'
-  }
-});
+ host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: account.user, // generated ethereal user
+            pass: account.pass  // generated ethereal password
+        }
+    });
 
 var mailOptions = {
   from: 'youremail@gmail.com',
